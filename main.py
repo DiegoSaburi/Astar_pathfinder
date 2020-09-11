6071028bc90536dd161baf337add847b5fc0e6e1
DiegoSaburi <diego_saburi@hotmail.com>
2020-09-11 17:57:21 -0300

    Alterações

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import math
+from queue import PriorityQueue
+from globals import *
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH,WIDTH))
+pygame.display.set_caption("A* Path Finding")
+
+print(RED)
---
+++ objects.py
@@ -0,0 +1,8 @@
+import pygame
+import math
+from queue import PriorityQueue
+from globals import *
+
+class Node:
+    def __init__(self):
+        super().__init__()
